97a9e66a3d1c32910bdccad645d838b5bf5b1db4
Artem <theuselessn@gmail.com>
2021-10-13 14:32:29 +0600

    pixel perfect started

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,9 @@
 const inputPhone = document.querySelector('#customer-phone');
+const pupupCloseButton = document.querySelector('.popul-call__button-close');
+const popupOpenButton = document.querySelector('.header__button-call');
+const popupOverlay = document.querySelector('.popup-call');
+
+const popupCloseClass = 'popup-call--closed';
 
 inputPhone.addEventListener('focus', (evt) => {
   if (evt.target.value.length === 0) {
@@ -12,11 +17,62 @@ inputPhone.addEventListener('focusout', (evt) => {
   }
 });
 
-// inputPhone.addEventListener('input', (evt) => {
-//   console.log(evt.target.value.length);
-//   if (evt.target.value.length === 6) {
-//     console.log(evt.target.value.length);
-//     evt.target.value = evt.target.value + ")";
-//   }
-// })
+// Popup
+const openPopup = () => {
+  popupOverlay.classList.remove(popupCloseClass);
+  const popupNameField = document.querySelector('#popup-name');
+  console.log(popupNameField);
+  popupNameField.focus();
+};
+
+const closePopup = () => {
+  popupOverlay.classList.add(popupCloseClass);
+};
+
+popupOpenButton.addEventListener('click', () => {
+  openPopup();
+});
+
+pupupCloseButton.addEventListener('click', () => {
+  closePopup();
+});
+
+popupOverlay.addEventListener('click', (evt) => {
+  if (evt.target === popupOverlay) {
+    closePopup();
+  }
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    closePopup();
+  }
+});
+// Accordeon
+const accordeonToggles = document.querySelectorAll('.accordeon__toggle');
+const accordeonBlocks = document.querySelectorAll('.accordeon__content');
+const ACTIVE_CLASS = 'accordeon__content--open';
+
+accordeonToggles.forEach((toggle) => {
+  toggle.addEventListener('click', (evt) => {
+    console.log(evt);
+    for (let i = 0; i < accordeonBlocks.length; i++) {
+      if (accordeonBlocks[i].dataset.accordeon === evt.target.dataset.accordeon) {
+        accordeonBlocks[i].classList.toggle(ACTIVE_CLASS);
+      } else {
+        accordeonBlocks[i].classList.remove(ACTIVE_CLASS);
+      }
+    }
+    if (evt.target.classList.contains('accordeon__toggle--open')) {
+      evt.target.classList.remove('accordeon__toggle--open');
+      console.log("HERE1");
+      return;
+    }
+    console.log("HERE2");
+    accordeonToggles.forEach((toggle) => {
+      toggle.classList.remove('accordeon__toggle--open');
+    });
+    evt.target.classList.add('accordeon__toggle--open');
+  })
+})
 
